c6f0b10495bf274e95e31e54f28266e98ce17ad3
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-08-27 02:32:55 -0400

    updates

---
+++ src/CommentDetails.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import { faker } from "@faker-js/faker";
+
+const CommentDetails = (props) => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img src={props.avatar} alt="avatar" />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          {props.author}
+        </a>
+        <div className="metadata">
+          <span className="date">{props.timeAgo}</span>
+        </div>
+        <div className="text">{props.comment}</div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentDetails;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import CommentDetails from "./CommentDetails";
+import ApprovalCard from "./ApprovalCard";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { faker } from "@faker-js/faker";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el);
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetails
+          author="Dmitriy"
+          timeAgo={faker.datatype
+            .datetime()
+            .toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
+          avatar={faker.image.avatar()}
+          comment="Hello my name is Dmitriy"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="Raymond"
+          timeAgo={faker.datatype
+            .datetime()
+            .toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
+          avatar={faker.image.avatar()}
+          comment="Hello my name is Raymond"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="Emilya"
+          timeAgo={faker.datatype
+            .datetime()
+            .toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
+          avatar={faker.image.avatar()}
+          comment="Hello my name is Emilya"
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+root.render(<App />);
